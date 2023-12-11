1de59bd1d197708511ac4a120585bcfbec90e1e5
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-11 17:33:20 +0000

    poc google cucumber tests

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,18 +1,35 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.After;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class StepDefinitions {
+    WebDriver driver = new ChromeDriver();
+
     @Given("I am on a chrome browser")
     public void i_am_on_a_chrome_browser() {
-        throw new io.cucumber.java.PendingException();
+        
     }
     @When("I get google")
     public void i_get_google() {
-        throw new io.cucumber.java.PendingException();
+        driver.get("https://www.google.co.uk");
     }
     @Then("I should see google title")
     public void i_should_see_google_title() {
-        throw new io.cucumber.java.PendingException();
+        driver.getTitle();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
     }
 }
